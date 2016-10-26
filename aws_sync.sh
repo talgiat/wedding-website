c48e16fdde7eb478431b2fe3b53b03bbf1eabd12
Tal Giat <tal.giat@thomsonreuters.com>
2016-10-25 20:33:37 -0400

    Updating styles adding sync to s3 script

---
+++ aws_sync.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws --profile talgiat $1 --exclude=aws_sync* --exclude=node_modules/* --exclude=.git* --exclude=package.json --exclude=index_* --exclude=.DS_Store s3 --exclude=*.txt sync . s3://anokhee-tal-wedding/
