c7e34c2e5ab78b6a5ad791416c51a715542653d7
Tal Giat <tal.giat@thomsonreuters.com>
2016-11-08 10:25:03 -0500

    Changing sync script to new site

--- aws_sync.sh
+++ aws_sync.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-aws --profile talgiat $1 --exclude=aws_sync* --exclude=node_modules/* --exclude=.git* --exclude=package.json --exclude=index_* --exclude=.DS_Store s3 --exclude=*.txt --cache-control=no-cache sync . s3://anokhee-tal-wedding/
+#anokhee-tal-wedding - real site
+#wedding.anokheetal.com - staging site
+echo "$1"
+aws --profile talgiat $1 --exclude=aws_sync* --exclude=node_modules/* --exclude=.git* --exclude=package.json --exclude=.DS_Store --exclude=*.txt --cache-control=no-cache s3 sync . s3://wedding.anokheetal.com/
