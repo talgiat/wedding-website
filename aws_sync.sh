f041254ef9e54d5b4b0e30e702442aae135a9e6d
Tal Giat <tal.giat@thomsonreuters.com>
2016-11-16 04:02:43 -0500

    First version of the actual site

--- aws_sync.sh
+++ aws_sync.sh
@@ -2,4 +2,4 @@
 #anokhee-tal-wedding - real site
 #wedding.anokheetal.com - staging site
 echo "$1"
-aws --profile talgiat $1 --exclude=aws_sync* --exclude=node_modules/* --exclude=.git* --exclude=package.json --exclude=.DS_Store --exclude=*.txt --cache-control=no-cache s3 sync . s3://wedding.anokheetal.com/
+aws --profile talgiat $1 --exclude=.sass_cache/* --exclude=sass/* --exclude=aws_sync* --exclude=node_modules/* --exclude=.git* --exclude=package.json --exclude=.DS_Store --exclude=*.txt --cache-control=no-cache s3 sync . s3://anokhee-tal-wedding/
